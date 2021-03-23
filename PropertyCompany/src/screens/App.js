17d9e389e0db6c6c54d55d004a0f3bde2afd60ab
YilKanda <yilkanda@hotmail.com>
2021-03-23 14:39:18 -0600

    Added icons
    Through the react-native-cross-platform-icons component, the necessary icons were added to simplify texts in the app.
    
    A react-native wrapper component that displays the appropriate icon based on the current platform ( Android and iOS )
    
    Installation
    
        npm install --save react-native-cross-platform-icons react-native-vector-icons
    
    After Installation (this step is required)
    
        react-native link react-native-vector-icons
    
    Usage
    
        import CrossPlatformIcon from 'react-native-cross-platform-icons';
    
        <CrossPlatformIcon name="home" size={45} color="black" outline />
        <CrossPlatformIcon name="arrow-back" size={40} color="#BADA55" />
    
    The indicative name, allows us to indicate the icon to be displayed, based on the page:
    
        https://ionicons.com/
    
    The size and color refer to that of the typographic font.
    
    The icons for the App were also added, from Android Studio.
    
    ToDo:
    
    — The position of the favorite icon must be corrected.
    — The onClick function must be implemented to the favorite icon.

--- PropertyCompany/src/screens/App.js
+++ PropertyCompany/src/screens/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { SafeAreaView, FlatList, StyleSheet, Text, View, ImageBackground } from 'react-native'
+import CrossPlatformIcon from 'react-native-cross-platform-icons'
 
 const App = () => {
   const renderItem = ({ item }) => (
@@ -26,15 +27,18 @@ const Item = ({ item }) => (
           style={ styles.photo }
           imageStyle={ styles.photo_imageStyle }>
           <View style={ styles.containerRating }>
-            <Text style={ styles.rating }>{ item.rating }</Text>
+            <Text style={ styles.rating }><CrossPlatformIcon name='star' size={12} color='#ffcb12'  /> { item.rating }</Text>
           </View>
         </ImageBackground>
       </View>
       <View style={ styles.containerText }>
         <Text style={ styles.name }>{ item.name }</Text>
-        <Text style={ styles.address }>{ item.address }</Text>
-        <Text style={ styles.number_rooms }>{ item.number_rooms }, { item.surface }</Text>
-        <Text style={ styles.monthly_rental_cost }>{ item.monthly_rental_cost }</Text>
+        <Text style={ styles.address }><CrossPlatformIcon name='location-outline' size={12} color='rgba(115, 115, 115, 1)'  />{ item.address }</Text>
+        <Text style={ styles.number_rooms }><CrossPlatformIcon name='bed-outline' size={12} color='rgba(115, 115, 115, 1)'  /> { item.number_rooms } <CrossPlatformIcon name='water-outline' size={12} color='rgba(115, 115, 115, 1)'  /> { item.number_bathrooms } <CrossPlatformIcon name='home-outline' size={12} color='rgba(115, 115, 115, 1)'  /> { item.surface } ft²</Text>
+        <View style={ styles.content_rental_cost }>
+          <Text style={ styles.monthly_rental_cost }>${ item.monthly_rental_cost }/m</Text>
+          <Text><CrossPlatformIcon style={ styles.favorite } name='heart-circle' size={28} color='#34b67a'  /></Text>
+        </View>
       </View>
     </View>
   </View>
@@ -81,7 +85,8 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 12,
     fontWeight: 'bold',
-    color: '#7A6229'
+    color: '#7A6229',
+    marginTop: 3
   },
   containerText: {
     marginLeft: 12
@@ -89,76 +94,100 @@ const styles = StyleSheet.create({
   name: {
     color: '#121212',
     fontSize: 18,
-    marginBottom: 10,
+    marginBottom: 7.5,
     fontWeight: 'bold'
   },
   address: {
     color: 'rgba(115, 115, 115, 1)',
     fontSize: 12,
-    marginBottom: 10
+    marginBottom: 7.5
   },
   number_rooms: {
     color: 'rgba(115, 115, 115, 1)',
     fontSize: 12,
-    marginBottom: 10,
-    fontWeight: 'bold'
+    marginBottom: 7.5,
+    fontWeight: 'bold',
+    letterSpacing: 2
+  },
+  content_rental_cost: {
+    flexDirection: 'row',
+    justifyContent:'space-between'
   },
   monthly_rental_cost: {
     color: 'rgba(21, 21, 37, 1)',
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+  },
+  favorite: {
+    textAlign: 'right'
   }
 })
 
 const DATA = [
   {
     id: 1,
-    name: 'Houses Name',
-    address: '001 Street',
+    name: 'Simpson House',
+    address: '742 Evergreen Terrace, Springfield',
     number_rooms: 1,
-    surface: 1,
-    monthly_rental_cost: 1,
-    rating: 1.0,
+    number_bathrooms: 2,
+    surface: 280,
+    monthly_rental_cost: 400,
+    rating: 5.0,
     favorite: false
   },
   {
     id: 2,
-    name: 'Houses Name',
-    address: '002 Street',
+    name: 'Griffin House',
+    address: '31 Spooner Street, Providence, RI',
     number_rooms: 2,
-    surface: 2,
-    monthly_rental_cost: 2,
+    number_bathrooms: 2,
+    surface: 270,
+    monthly_rental_cost: 500,
     rating: 2.0,
     favorite: false
   },
   {
     id: 3,
-    name: 'Houses Name',
-    address: '002 Street',
+    name: 'Smith House',
+    address: '570 Wilshire Blvd, Los Angeles, CA',
     number_rooms: 2,
-    surface: 2,
-    monthly_rental_cost: 2,
-    rating: 2.0,
+    number_bathrooms: 3,
+    surface: 260,
+    monthly_rental_cost: 700,
+    rating: 1.0,
     favorite: false
   },
   {
     id: 4,
-    name: 'Houses Name',
-    address: '002 Street',
+    name: 'Shortman House',
+    address: '4040 Vine Street, Riverside, CA',
     number_rooms: 2,
-    surface: 2,
-    monthly_rental_cost: 2,
-    rating: 2.0,
+    number_bathrooms: 1,
+    surface: 250,
+    monthly_rental_cost: 600,
+    rating: 4.0,
     favorite: false
   },
   {
     id: 5,
-    name: 'Houses Name',
-    address: '002 Street',
+    name: 'Parker House',
+    address: '20 Ingram Street, Flushing, NY',
     number_rooms: 2,
-    surface: 2,
-    monthly_rental_cost: 2,
-    rating: 2.0,
+    number_bathrooms: 1,
+    surface: 240,
+    monthly_rental_cost: 500,
+    rating: 3.0,
+    favorite: false
+  },
+  {
+    id: 6,
+    name: 'The Willows',
+    address: '3517 W. Gray St. Utica',
+    number_rooms: 3,
+    number_bathrooms: 2,
+    surface: 230,
+    monthly_rental_cost: 440,
+    rating: 4.7,
     favorite: false
   }
 ]
